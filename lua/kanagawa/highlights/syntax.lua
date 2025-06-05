5ce2f55b30a7888752f29b067ce2be4a800ab765
Sergey Kuzin <sergeykuzin2008@ya.ru>
2025-06-05 13:41:08 +0300

    13-41

--- lua/kanagawa/highlights/syntax.lua
+++ lua/kanagawa/highlights/syntax.lua
@@ -14,62 +14,62 @@ function M.setup(colors, config)
         -- *Constant	any constant
         Constant = { fg = theme.syn.constant, bold = false, italic = false },
         --  String		a string constant: "this is a string"
-        String = { fg = theme.syn.string, bold = false },
+        String = { fg = theme.syn.string, bold = false, italic = false },
         --  Character	a character constant: 'c', '\n'
-        Character = { link = "String", bold = false },
+        Character = { link = "String", bold = false, italic = false },
         --  Number		a number constant: 234, 0xff
-        Number = { fg = theme.syn.number, bold = false },
+        Number = { fg = theme.syn.number, bold = false, italic = false },
         --  Boolean	a boolean constant: TRUE, false
-        Boolean = { fg = theme.syn.constant, bold = false },
+        Boolean = { fg = theme.syn.constant, bold = false, italic = false },
         --  Float		a floating point constant: 2.3e10
-        Float = { link = "Number", bold = false },
+        Float = { link = "Number", bold = false, italic = false },
 
         -- *Identifier	any variable name
-        Identifier = { fg = theme.syn.identifier, bold = false },
+        Identifier = { fg = theme.syn.identifier, bold = false, italic = false },
         --  Function	function name (also: methods for classes)
-        Function = vim.tbl_extend("force", { fg = theme.syn.fun, bold = false }, config.functionStyle),
+        Function = vim.tbl_extend("force", { fg = theme.syn.fun, bold = false, italic = false }, config.functionStyle),
 
         -- *Statement	any statement
-        Statement = vim.tbl_extend("force", { fg = theme.syn.statement, bold = false }, config.statementStyle),
+        Statement = vim.tbl_extend("force", { fg = theme.syn.statement, bold = false, italic = false }, config.statementStyle),
         --  Conditional	if, then, else, endif, switch, etc.
         --  Repeat		for, do, while, etc.
         --  Label		case, default, etc.
         --  Operator	"sizeof", "+", "*", etc.
-        Operator = { fg = theme.syn.operator, bold = false },
+        Operator = { fg = theme.syn.operator, bold = false, italic = false },
         --  Keyword	any other keyword
-        Keyword = vim.tbl_extend("force", { fg = theme.syn.keyword, bold = false }, config.keywordStyle),
+        Keyword = vim.tbl_extend("force", { fg = theme.syn.keyword, bold = false, italic = false }, config.keywordStyle),
         --  Exception	try, catch, throw
-        Exception = { fg = theme.syn.special2, bold = false },
+        Exception = { fg = theme.syn.special2, bold = false, italic = false },
 
         -- *PreProc	generic Preprocessor
-        PreProc = { fg = theme.syn.preproc, bold = false },
+        PreProc = { fg = theme.syn.preproc, bold = false, italic = false },
         --  Include	preprocessor #include
         --  Define		preprocessor #define
         --  Macro		same as Define
         --  PreCondit	preprocessor #if, #else, #endif, etc.
 
         -- *Type		int, long, char, etc.
-        Type = vim.tbl_extend("force", { fg = theme.syn.type, bold = false }, config.typeStyle),
+        Type = vim.tbl_extend("force", { fg = theme.syn.type, bold = false, italic = false }, config.typeStyle),
         --  StorageClass	static, register, volatile, etc.
         --  Structure	struct, union, enum, etc.
         --  Typedef	A typedef
 
         -- *Special	any special symbol
-        Special = { fg = theme.syn.special1, bold = false },
+        Special = { fg = theme.syn.special1, bold = false, italic = false },
         --  SpecialChar	special character in a constant
         --  Tag		you can use CTRL-] on this
         --  Delimiter	character that needs attention
-        Delimiter = { fg = theme.syn.punct, bold = false },
+        Delimiter = { fg = theme.syn.punct, bold = false, italic = false },
         --  SpecialComment	special things inside a comment
         --  Debug		debugging statements
 
         -- *Underlined	text that stands out, HTML links
-        Underlined = { fg = theme.syn.special1, underline = true, bold = false },
+        Underlined = { fg = theme.syn.special1, underline = true, bold = false, italic = false },
         Bold = { bold = true },
         Italic = { italic = true },
 
         -- *Ignore		left blank, hidden  |hl-Ignore|
-        Ignore = { link = "NonText", bold = false },
+        Ignore = { link = "NonText", bold = false, italic = false },
 
         -- *Error		any erroneous construct
         Error = { fg = theme.diag.error },
@@ -77,8 +77,8 @@ function M.setup(colors, config)
         -- *Todo		anything that needs extra attention; mostly the keywords TODO FIXME WARNING and XXX
         Todo = { fg = theme.ui.fg_reverse, bg = theme.diag.info, bold = true },
 
-        qfLineNr = { link = "lineNr", bold = false },
-        qfFileName = { link = "Directory", bold = false },
+        qfLineNr = { link = "lineNr", bold = false, italic = false },
+        qfFileName = { link = "Directory", bold = false, italic = false },
 
         -- htmlH1 = {},
         -- htmlH2 = {},
